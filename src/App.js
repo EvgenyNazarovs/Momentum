3d89f2c708cee4e4242332c96b37d29b43236f79
Alex Gyllos <alex.gyllos@gmail.com>
2020-06-20 14:48:00 +0100

    adds sounds

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, {useState, useEffect} from 'react';
 import PoseNet from './components/PoseNet'
 import './App.css';
 import NavBar from './components/NavBar.js'
+import backgroundSounds from './sounds/background.mp3'
+import cNote from './sounds/CNote.mp3'
+import gNote from './sounds/GNote.mp3'
+import aNote from './sounds/ANote.mp3'
+import dNote from './sounds/DNote.mp3'
 
 function App() {
   const [posesString, setPosesString] = useState([])
 
-
-
   useEffect(() => {
-
+    const backgroundSounds = document.getElementById("background-sounds");
+    backgroundSounds.play()
+    const cNote = document.getElementById("c-note");
+    const gNote = document.getElementById("g-note");
+    const dNote = document.getElementById("d-note");
+    const aNote = document.getElementById("a-note");
 
   if (posesString.length !== 0) {
       if (posesString[0].part === 'nose') {
@@ -18,8 +26,10 @@ function App() {
 
         if (noseX > 50 && noseX < 250 && noseY > 50 && noseY < 250) {
           console.log('youre in the frame')
+          cNote.play()
         } else {
           console.log('now youre not.');
+          gNote.play()
         }
       }
     }}, [posesString])
@@ -34,6 +44,11 @@ function App() {
               if (poses.length !== 0) setPosesString(poses[0].keypoints)
         }}
       />
+      <audio id="background-sounds" src={backgroundSounds}></audio>
+      <audio id="c-note" src={cNote}></audio>
+      <audio id="g-note" src={gNote}></audio>
+      <audio id="d-note" src={dNote}></audio>
+      <audio id="a-note" src={aNote}></audio>
     </div>
   );
 }
