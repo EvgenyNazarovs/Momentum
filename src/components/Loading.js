3914e5ba88100338086cb0f569bbf4f69b3f015a
Michael Wales <michaelwales@gmail.com>
2020-06-23 22:03:00 +0100

    Adds Spinner for 13inch screens

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,10 @@ import '../App.css'
 
 export default function({ name, target }) {
   if (!target) {
-    return <img src={logo} className="App-logo" alt="logo" />
+    return <div className="App-logo-div">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2 className="App-logo-text">loading...please be patient...</h2>
+    </div>
   }
   if (target instanceof Error) {
     return (
